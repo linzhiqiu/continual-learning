f7d3741106c755c606b878b3a849f1a7546c8b19
linzhiqiu <zl279@cornell.edu>
2021-12-12 00:24:27 -0500

    [update] clear 50 prompts

--- train_single_alpha.sh
+++ train_single_alpha.sh
@@ -1,14 +1,14 @@
 # Offline experiment
 python train_single_alpha.py --alpha_value_mode fixed --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
 python train_single_alpha.py --alpha_value_mode fixed --seed 1 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode fixed --seed 10 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode fixed --seed 100 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode fixed --seed 1000 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode dynamic --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode dynamic --seed 1 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode dynamic --seed 10 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode dynamic --seed 100 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
-python train_single_alpha.py --alpha_value_mode dynamic --seed 1000 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=1 python train_single_alpha.py --alpha_value_mode fixed --seed 10 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=1 python train_single_alpha.py --alpha_value_mode fixed --seed 100 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=1 python train_single_alpha.py --alpha_value_mode fixed --seed 1000 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=2 python train_single_alpha.py --alpha_value_mode dynamic --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=2 python train_single_alpha.py --alpha_value_mode dynamic --seed 1 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=2 python train_single_alpha.py --alpha_value_mode dynamic --seed 10 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=3 python train_single_alpha.py --alpha_value_mode dynamic --seed 100 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
+CUDA_VISIBLE_DEVICES=3 python train_single_alpha.py --alpha_value_mode dynamic --seed 1000 --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned
 
 
 python train_single_alpha.py --alpha_value_mode fixed --dataset_name dynamic_300 --mode no_test_set --train_mode moco_v2_yfcc_feb18_bucket_0_gpu_8_linear_tuned_batch_8
